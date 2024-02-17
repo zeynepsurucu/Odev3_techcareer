d021958e6bf8ab1ecb90b7d7994d0d3982971b78
zeyneps <zeynepsurucu.dev@gmail.com>
2024-02-17 20:55:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/odev3/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.odev3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.odev3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
